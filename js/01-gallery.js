0b699bd92c5f8542b304141b3417f7384d38806e
LenaMaistrenko <istlauder@gmail.com>
2022-12-26 16:55:50 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+console.log(galleryContainer);
 
-console.log(galleryItems);
+function createMarkup(arr) {
+  const markup = arr
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+
+  return markup;
+}
+galleryContainer.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+console.log("galleryItems", galleryItems);
