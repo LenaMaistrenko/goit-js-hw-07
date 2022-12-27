495e74b95f000301808718593746ab93e731fa3b
LenaMaistrenko <istlauder@gmail.com>
2022-12-27 11:25:24 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,19 +7,38 @@ function createMarkup(arr) {
   const markup = arr
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
+        <a class="gallery__link" href="${original}">
+        <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"/> 
+         </a>
+      </div>`;
     })
     .join("");
 
   return markup;
 }
 galleryContainer.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+galleryContainer.addEventListener("click", galleryOnClick);
+
+function galleryOnClick(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  evt.preventDefault();
+  const imgUrl = evt.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src= "${imgUrl}">`);
+  instance.show();
+  window.addEventListener("keydown", onEscPress);
+  function onEscPress(evt) {
+    console.log(evt.code);
+    if (evt.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onEscPress);
+    }
+  }
+}
 console.log("galleryItems", galleryItems);
